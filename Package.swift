2eea9451ef0e7182c973e60e90ea981304d54260
Ian Keen <iankeen82@gmail.com>
2025-11-16 18:55:18 -0800

    Update Package.swift for binary distribution v1.0.0

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "NextUpSDK",
             url: "https://github.com/timberlanelabs/NextUp-iOS/releases/download/1.0.0/NextUpSDK.xcframework.zip",
-            checksum: "60d275b9f5b9d198b4cd59be1a18575b5350b712ce259816966f07bf9a938dd7"
+            checksum: "0c6e01a50a360cb5faddd989eab7ef486d4efd79dccdf000d0c390e88e253f8f"
         )
     ]
 )
